8d228878d348fadb2bab8ffcafed9f7e357b17c4
Matias <malvarado22@cmc.edu>
2020-02-23 15:50:06 -0800

    Created word ladder functions

--- word_ladder.py
+++ word_ladder.py
@@ -1,5 +1,11 @@
 #!/bin/python3
 
+from collections import deque
+import copy 
+
+
+
+
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
@@ -29,12 +35,54 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     the function returns `None`.
     '''
 
+    if start_word == end_word:
+        return start_word
+
+    myStack = []
+    myStack.append(start_word)
+
+    myQue = deque()
+    myQue.appendleft(myStack)
+    
+    myWordFile = open(dictionary_file).readlines()
+    wordCollection = []
+   
+    for x in myWordFile:
+       wordCollection.append(x.strip("\n"))    #removes any trailing (\n) chars
+
+    #lengthOfMyQue = len(myQue)
+    while len(myQue)!= 0:
+        edit = myQue.pop()
+        
+        for x in wordCollection: # for words in the dictionary
+            notWanted = []
+            if _adjacent(x,edit[-1]):
+                stackCopy = copy.deepcopy(edit)
+                stackCopy.append(x)
+                if x == end_word:
+                    lengthOfStackCopy = len(stackCopy)
+                    for y in range(1,lengthOfStackCopy-2):
+                        if -adjacent(edit[y - 1],edit[ y + 1]):
+                            stackCopy.pop(y)
+                    return (stackCopy)
+                myQue.appendleft(stackCopy)
+                wordCollection.remove(x)
+            
+        
 
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
     otherwise returns False.
     '''
+
+    if ladder == []:
+        return False
+
+    for word1,word2 in zip(ladder,ladder[1:]):
+        if not _adjacent(word1, word2):
+            return False
+    return True
 
 
 def _adjacent(word1, word2):
@@ -47,3 +95,29 @@ def _adjacent(word1, word2):
     >>> _adjacent('stone','money')
     False
     '''
+    count = 0
+    length1 = len(word1)
+    length2 = len(word2)
+
+    if word1 == word2:
+        return False
+    
+    if length1 == length2:
+        for x,y in zip(word1,word2):
+            if x != y:
+                count += 1
+
+    if (count == 1):
+        return True
+    else:
+        return False
+
+
+
+
+
+
+
+
+
+
